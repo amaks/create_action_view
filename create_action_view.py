c84e878082236650cbfa93061441525855a1bb81
Andrew Maksimov <maksimov.andrew@gmail.com>
2019-11-15 14:30:06 +0200

    [New feature] create views folder for controller of absent

--- create_action_view.py
+++ create_action_view.py
@@ -4,28 +4,37 @@ import sublime, sublime_plugin, os
 class CreateActionViewCommand(sublime_plugin.TextCommand):
   def run(self, edit):
 
-    region          = self.view.sel()[0]
-    source          = self.view.file_name()
-    source_path     = os.path.dirname(source)
-    rails_view_path = os.path.dirname(source_path)
-    views_path      = source.replace('controllers', 'views').split('_controller.rb')[0]
-    actions         = []
-    all_defs        = self.view.find_all(' def ')
+    region      = self.view.sel()[0]
+    file_name   = self.view.file_name()
+    views_path  = file_name.replace('controllers', 'views').split('_controller.rb')[0]
+    extension   = self.detect_extensiton(file_name)
+    action_name = self.detect_action_name(region)
+    file_path   = views_path + '/' + action_name + '.' + extension
 
+    self.create_view_folder(views_path)
+    self.create_view_file(file_path)
+
+  def detect_action_name(self, region):
+    all_defs    = self.view.find_all(' def ')
     for d in all_defs:
       if d < region:
         line        = self.view.line(d)
         line_text   = self.view.substr(line)
-        action_name = line_text.split('def ')[1].strip()
-        actions.append(action_name)
+        return line_text.split('def ')[1].strip()
 
+  def detect_extensiton(self, file_name):
+    file_path       = os.path.dirname(file_name)
+    rails_view_path = os.path.dirname(file_path)
     layouts_folder = rails_view_path + '/views/layouts/'
     for file in os.listdir(layouts_folder):
-        if 'application' in file:
-            extension = file.split('.')[1]
+      if 'application' in file:
+        return file.split('.')[1]
 
-    views_path_with_file_name = views_path + '/' + action_name + '.' + extension
+  def create_view_folder(self, views_path):
+    if not os.path.exists(views_path):
+      os.mkdir(views_path)
 
-    if not os.path.exists(views_path_with_file_name):
-      open(views_path_with_file_name, 'w')
-      self.view.window().open_file(views_path_with_file_name)
+  def create_view_file(self, file_path):
+    if not os.path.exists(file_path):
+      open(file_path, 'w')
+      self.view.window().open_file(file_path)
