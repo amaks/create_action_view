c510c20d971df1fa44790e6cdfc73d8bbdabc0e7
Andrew Maksimov <maksimov.andrew@gmail.com>
2019-11-17 15:04:57 +0200

    []

--- create_action_view.py
+++ create_action_view.py
@@ -1,26 +1,32 @@
 import sublime, sublime_plugin, os
-# super+shift+8
+# super + alt + k
+
+_viewPath = None
 
 class CreateActionViewCommand(sublime_plugin.TextCommand):
   def run(self, edit):
+    global _viewPath
 
-    region      = self.view.sel()[0]
-    file_name   = self.view.file_name()
-    views_path  = file_name.replace('controllers', 'views').split('_controller.rb')[0]
-    extension   = self.detect_extensiton(file_name)
-    action_name = self.detect_action_name(region)
-    file_path   = views_path + '/' + action_name + '.' + extension
+    region    = self.view.sel()[0]
+    file_name = self.view.file_name()
+    _viewPath = file_name.replace('controllers', 'views').split('_controller.rb')[0]
+    extension = self.detect_extensiton(file_name)
+    all_defs  = self.view.find_all(' def ')
 
-    self.create_view_folder(views_path)
-    self.create_view_file(file_path)
-
-  def detect_action_name(self, region):
-    all_defs    = self.view.find_all(' def ')
     for d in all_defs:
       if d < region:
         line        = self.view.line(d)
         line_text   = self.view.substr(line)
-        return line_text.split('def ')[1].strip()
+        action_name = line_text.split('def ')[1].strip()
+
+    self.view.window().show_input_panel("File with extension: ", action_name + '.' + extension, self.get_selected_text, None, None)
+
+  def get_selected_text(self, file_with_extension):
+    global _viewPath
+
+    file_path = _viewPath + '/' + file_with_extension
+    self.create_view_folder(_viewPath)
+    self.create_view_file(file_path)
 
   def detect_extensiton(self, file_name):
     file_path       = os.path.dirname(file_name)
